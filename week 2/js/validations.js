47e8ccd9c6fec1f7d93788a50058e50a981bb163
Bas <b.d.r._bas@live.nl>
2017-09-28 20:00:37 +0200

    finished opdracht 2

---
+++ week 2/js/validations.js
@@ -0,0 +1,24 @@
+function validateName() {
+    var x = document.getElementById("name").value;
+    if (x == "") {
+        alert("Name must be filled out");
+    }
+}
+function validateStreet() {
+    var x = document.getElementById("street").value;
+    if (x == "") {
+        alert("Street must be filled out");
+    }
+}
+function validateCity() {
+    var x = document.getElementById("city").value;
+    if (x == "") {
+        alert("City must be filled out");
+    }
+}
+function validatePhone() {
+    var x = document.getElementById("phone").value;
+    if (x == "") {
+        alert("Phone must be filled out");
+    }
+}
